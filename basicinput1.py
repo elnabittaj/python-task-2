117b721321873c85e6f31df78d82782f4d35a430
elnabittaj <elnabittaj@gmail.com>
2024-10-18 15:09:32 +0530

    Add files via upload

---
+++ basicinput1.py
@@ -0,0 +1,10 @@
+'''
+Author:Elna Ann Bittaj
+Date:18-10-2024
+Description:Python program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. Finally, display the new string.
+'''
+str_var="Hello World"
+str_var1=str_var[6:]
+#print(str_var1)
+str_var2=" Everyone!"
+print(str_var1+str_var2)
